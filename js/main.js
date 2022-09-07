f3c8ff1a4544db1bf22c35b01f7aa2b1e7220132
Juan Martín Núñez Aréchiga <jmartin.nuar@gmail.com>
2022-09-07 15:04:42 -0500

    chore | Se agrega el algoritmo 1 a main.js

--- js/main.js
+++ js/main.js
@@ -1,28 +1,51 @@
-// Ejercicio 1
+// Algoritmo 1
 
-// Se define una función que tome arreglo de números y devuelva el número mayor de la lista.
+// 1. Solicitar 3 números (entre el 1 y el 100)  y definir cual es el mayor
 
-// 1. Se define el arreglo de entrada
 
-let arreglo = [3, 5, 7, 1, 6];
 
-// 2. Se calcula el número mayor con comparaciones en un ciclo
+let btnRes = document.getElementById("btnRes");
 
-function numeroMayor(max) {
-    // 3. Se asigna el primer valor del arreglo
 
-    max = arreglo[0];
+btnRes.addEventListener("click", function (e) {
+    e.preventDefault();
+    
+    let n1 = parseInt(document.getElementById("numero1").value);
+    let n2 = parseInt(document.getElementById("numero2").value);
+    let n3 = parseInt(document.getElementById("numero3").value);
+    let res = document.getElementById("respuesta");
+    let flag = true;
 
-    // 4. Se comienza a comparar con el segundo valor del arreglo y continúa hasta recorrer todo el ciclo
-
-    for (let cont = 1; cont < arreglo.length; cont++) {
-        if (max < arreglo[cont]) {
-            max = arreglo[cont];
-        }
+    res.innerHTML = "";
+    if (isNaN(n1) || isNaN(n2) || isNaN(n3)) {
+        res.innerHTML = `Favor de no dejar campos vacíos ni utilizar letras o símbolos`;
+        flag = false;
     }
-    return max;
-}
 
-// 5. Devuelve el resultado en la consola
+    let max = n1;
+    if (max < n3) {
+            max = n3;
+        }
+    if (max < n2) {
+            max = n2;
+        }
+    
 
-console.log(numeroMayor(arreglo));
+    if (n1 < 1 || n1 > 100) {
+        res.innerHTML += `El número ${n1} no se encuentra en el rango especificado<br>`;
+        flag = false;
+    }
+    if (n2 < 1 || n2 > 100) {
+        res.innerHTML += `El número ${n2} no se encuentra en el rango especificado<br>`;
+        flag = false;
+    }
+    if (n3 < 1 || n3 > 100) {
+        res.innerHTML += `El número ${n3} no se encuentra en el rango especificado<br>`;
+        flag = false;
+    }
+    if (flag) {
+        res.innerHTML = `El número mayor es ${max}`;
+    }
+
+    }
+);
